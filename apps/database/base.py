2e6cc67cbcdd1cfa4ff0a1452be1edac7e8a174f
Lee Hyun Jun <bhk0827@gmail.com>
2023-09-07 23:59:18 +0900

    <Feat> : base for model, schema
    a

---
+++ apps/database/base.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, DateTime
+from sqlalchemy.sql import func
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+
+
+@as_declarative()
+class ModelBase:
+    __name__: str
+    __abstract__ = True
+    id = Column(Integer, primary_key=True)
+    created_time = Column(DateTime, default=func.now(), nullable=True)
+    modified_time = Column(DateTime, default=None, onupdate=func.now(), nullable=True)
+
+    # Generate __tablename__ auto
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+    def to_dict(self):
+        return {
+            column.name: getattr(self, column.name) for column in self.__table__.columns
+        }
