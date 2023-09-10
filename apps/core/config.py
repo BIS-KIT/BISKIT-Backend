933a998bdd51be9beca7371af498856311fe8bc2
Lee Hyun Jun <bhk0827@gmail.com>
2023-09-11 02:53:51 +0900

    <Feat> : firebase sdk setting
    - fastapi config setting 수정
    - sdk 파일 base64인코

--- apps/core/config.py
+++ apps/core/config.py
@@ -1,13 +1,21 @@
 from typing import List, Optional
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 
 class Setting(BaseSettings):
+    PROJECT_NAME: str
     BISKIT_USER: str
     BISKIT_USER_PW: str
     POSTGRES_DB: str
     DB_ROOT_PASSWORD: str
-    CORS_ORIGINS: List[str]
+    CORS_ORIGINS: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    @property
+    def cors_origins_list(self) -> List[str]:
+        return self.CORS_ORIGINS.split(",")
 
     class Config:
         env_file = ".env"
--- apps/main.py
+++ apps/main.py
@@ -1,13 +1,28 @@
+import base64, json
+from dotenv import load_dotenv
+
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from firebase_admin import credentials, initialize_app
 
-from apps.core.config import settings
-from dotenv import load_dotenv
+from core.config import settings
+from api.v1.router import api_router as v1_router
+
+
+with open("encoded_key.txt", "r") as file:
+    encoded_data = file.read()
+
+decoded_data = base64.b64decode(encoded_data).decode("utf-8")
+firebase_config = json.loads(decoded_data)
+# Firebase 초기화
+cred = credentials.Certificate(firebase_config)
+initialize_app(cred)
 
 load_dotenv()
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
+app.include_router(v1_router, prefix="/v1")
 # Set all CORS enabled origins
 if settings.CORS_ORIGINS:
     app.add_middleware(
