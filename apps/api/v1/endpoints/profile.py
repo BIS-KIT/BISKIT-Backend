8950155ef5d912de170aa2bd8c2e36b73f124385
Lee Hyun Jun <bhk0827@gmail.com>
2023-09-14 02:36:00 +0900

    <Feat> : register Profile

--- apps/api/v1/endpoints/profile.py
+++ apps/api/v1/endpoints/profile.py
@@ -1,11 +1,22 @@
+import httpx, shutil
 from typing import Any, List, Optional, Dict
 
-from fastapi import APIRouter, Body, Depends, HTTPException, Header
+from fastapi import (
+    APIRouter,
+    Body,
+    Depends,
+    HTTPException,
+    Header,
+    File,
+    UploadFile,
+    Path,
+)
 from pydantic.networks import EmailStr
 from sqlalchemy.orm import Session
 
 import crud
 from database.session import get_db
+from core.config import settings
 from models.user import User
 from models.profile import Profile
 from schemas.profile import ProfileCreate, ProfileUpdate, ProfileResponse, ProfileBase
@@ -13,12 +24,51 @@ from schemas.profile import ProfileCreate, ProfileUpdate, ProfileResponse, Profi
 router = APIRouter()
 
 
+@router.get("/profile/{user_id}/", response_model=ProfileBase)
+def get_profile_by_user_id(
+    user_id: int = Path(..., title="The ID of the user"), db: Session = Depends(get_db)
+):
+    """
+    user_id를 이용하여 프로필 정보를 가져옵니다.
+
+    매개변수:
+    - user_id (int): 프로필 정보를 검색할 사용자의 ID.
+    - db (Session): 데이터베이스 세션.
+
+    반환값:
+    - ProfileBase: 지정된 user_id에 해당하는 사용자의 프로필 정보.
+    """
+    db_profile = crud.profile.get_by_user_id(db, user_id=user_id)
+    if not db_profile:
+        raise HTTPException(status_code=404, detail="Profile not found")
+    return db_profile
+
+
 @router.post("/profile/", response_model=ProfileBase)
 def create_profile(
     profile: ProfileCreate,
-    user_id: int,  # 사용자 ID를 추가로 받습니다.
+    user_id: int,
     db: Session = Depends(get_db),
 ):
+    """
+    사용자 프로필 생성 API
+
+    해당 API는 주어진 사용자 ID에 대한 프로필을 생성합니다.
+    사용자 ID가 존재하지 않거나 이미 프로필이 있는 경우에는 오류를 반환합니다.
+    닉네임이 제공되지 않은 경우, 외부 API를 통해 닉네임을 자동으로 생성합니다.
+
+    Required:
+    - first_name
+    - last_name
+    - gender
+
+    Parameters:
+    - profile: Profile 생성을 위한 정보.
+    - user_id: 프로필을 생성하려는 사용자의 ID.
+
+    Returns:
+    - 생성된 프로필 정보.
+    """
     # 유저 존재 확인
     user = db.query(User).filter(User.id == user_id).first()
     if not user:
@@ -30,20 +80,26 @@ def create_profile(
             status_code=400, detail="Profile already exists for the user"
         )
 
-    new_profile = ProfileCreate(
+    nick_name = profile.nick_name
+    if not nick_name:
+        # 닉네임을 생성하기 위해 외부 API에 요청
+        response = httpx.get(settings.nickname_api)
+        response.raise_for_status()  # 오류가 발생하면 예외를 발생시킵니다.
+        nick_name = response.json()["words"][0]
+
+    obj_in = ProfileCreate(
         first_name=profile.first_name,
         last_name=profile.last_name,
+        nick_name=nick_name,
         birth=profile.birth,
         nationality=profile.nationality,
         gender=profile.gender,
         is_graduated=profile.is_graduated,
         university=profile.university,
         department=profile.department,
-        user_id=user_id,  # 프로필의 user_id 설정
     )
-    db.add(new_profile)
-    db.commit()
-    db.refresh(new_profile)
+
+    new_profile = crud.profile.create(db=db, obj_in=obj_in, user_id=user_id)
     return new_profile
 
 
@@ -64,3 +120,27 @@ def update_profile(
     )
 
     return updated_profile
+
+
+@router.delete("/profile/{profile_id}", response_model=ProfileResponse)
+def delete_profile(profile_id: int, db: Session = Depends(get_db)):
+    """
+    프로필 삭제 API
+    """
+    profile = crud.profile.get(db, id=profile_id)
+    if not profile:
+        raise HTTPException(status_code=404, detail="Profile not found")
+    return crud.profile.remove(db, id=profile_id)
+
+
+@router.delete("/profile/user/{user_id}", response_model=ProfileBase)
+def delete_profile_by_user(user_id: int, db: Session = Depends(get_db)):
+    """
+    사용자 ID를 기반으로 프로필 삭제 API
+    """
+    profile = crud.profile.get_by_user_id(db, user_id=user_id)
+    if not profile:
+        raise HTTPException(
+            status_code=404, detail="Profile not found for the given user_id"
+        )
+    return crud.profile.remove(db, id=profile.id)
--- apps/api/v1/router.py
+++ apps/api/v1/router.py
@@ -6,4 +6,4 @@ from api.v1.endpoints import profile
 api_router = APIRouter()
 
 api_router.include_router(user.router, tags=["user"])
-api_router.include_router(profile.router, tags=["user"])
+api_router.include_router(profile.router, tags=["profile"])
