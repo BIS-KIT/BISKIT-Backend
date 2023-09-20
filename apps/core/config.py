c4cd0451b0875a1d05ea168bd7a21e6d2103302e
Lee Hyun Jun <bhk0827@gmail.com>
2023-09-20 10:43:22 +0900

    <Fix> : Add bucket-name in settings

--- apps/core/config.py
+++ apps/core/config.py
@@ -25,6 +25,7 @@ class Setting(BaseSettings):
     AWS_ACCESS_KEY_ID: str
     AWS_SECRET_ACCESS_KEY: str
     AWS_REGION: str
+    BUCKET_NAME: str
 
     @property
     def cors_origins_list(self) -> List[str]:
