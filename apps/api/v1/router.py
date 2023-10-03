411e81f4e06e9535ece6252f5adf42b9abacda85
Lee Hyun Jun <bhk0827@gmail.com>
2023-10-03 19:47:37 +0900

    <Recfactoring> : seperate login api from user

--- apps/api/v1/router.py
+++ apps/api/v1/router.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from api.v1.endpoints import user, profile, utility
+from api.v1.endpoints import user, profile, utility, login
 
 api_router = APIRouter()
 
+api_router.include_router(login.router, tags=["login"])
 api_router.include_router(user.router, tags=["user"])
 api_router.include_router(profile.router, tags=["profile"])
 api_router.include_router(utility.router, tags=["utility"])
