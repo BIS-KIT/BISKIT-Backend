25485b5ca3b592c4113f038e4882b038fb6b6f24
Lee Hyun Jun <bhk0827@gmail.com>
2023-10-14 20:45:04 +0900

    <Feat> : photo list custom

--- apps/admin/base.py
+++ apps/admin/base.py
@@ -3,12 +3,22 @@ from sqladmin import ModelView, Admin
 from models.user import User
 from models.profile import StudentVerification
 
+import os
+from pathlib import Path
+from fastapi.templating import Jinja2Templates
+
+current_file_path = os.path.abspath(__file__)
+folder_path = os.path.dirname(os.path.dirname(current_file_path))
+
+templates_dir = f"{folder_path}/admin/templates"
+
 
 class UserAdmin(ModelView, model=User):
     column_list = [User.id, User.name]
 
 
 class StudentVerificationAdmin(ModelView, model=StudentVerification):
+    list_template = "photo_list.html"
     column_list = [
         StudentVerification.student_card,
         StudentVerification.verification_status,
