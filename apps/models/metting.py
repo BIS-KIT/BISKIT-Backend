fd86478ec76aa3ad237e4b209f53b599363ed17c
Lee Hyun Jun <bhk0827@gmail.com>
2023-10-25 21:26:12 +0900

    <Feat> : Model for metting

---
+++ apps/models/metting.py
@@ -0,0 +1,45 @@
+from sqlalchemy import Column, Integer, String,ForeignKey, DateTime, Boolean
+from sqlalchemy.orm import relationship
+
+from models.base import ModelBase
+
+class Metting(ModelBase):
+    name = Column(String)
+    location = Column(String,nullable=True)
+    description = Column(String,nullable=True)
+    metting_time = Column(DateTime, nullable=True)
+    max_participants = Column(String)
+    current_participants = Column(String,nullable=True)
+    participants_status = Column(String,nullable=True)
+
+    image_url = Column(String)
+    is_active = Column(Boolean)
+
+    metting_tags = relationship("MeetingTag", back_populates="meeting")
+    metting_languages = relationship("MeetingLanguage", back_populates="meeting")
+
+
+class MeetingLanguage(ModelBase):
+    
+    meeting_id = Column(Integer, ForeignKey('meeting.id'))
+    language_id = Column(Integer, ForeignKey('language.id'))
+
+    meeting = relationship("Meeting", back_populates="metting_languages")
+    language = relationship("Language", back_populates="metting_languages")
+
+class MeetingTag(ModelBase):
+    
+    meeting_id = Column(Integer, ForeignKey("meeting.id"), primary_key=True)
+    tag_id = Column(Integer, ForeignKey("tag.id"), primary_key=True)
+    
+    meeting = relationship("Meeting", back_populates="metting_tags")
+    tag = relationship("Tag", back_populates="metting_tags")
+
+
+class MeetingTopic(ModelBase):
+    
+    meeting_id = Column(Integer, ForeignKey("meetings.id"), primary_key=True)
+    topic_id = Column(Integer, ForeignKey("topics.id"), primary_key=True)
+    
+    meeting = relationship("Meeting", back_populates="metting_topics")
+    topic = relationship("Topic", back_populates="metting_topics")
