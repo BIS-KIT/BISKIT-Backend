c500aef5e9a72214d3cdfa875ade03d122d42754
Lee Hyun Jun <bhk0827@gmail.com>
2023-10-28 13:15:17 +0900

    <Feat> : meeting api 추가

--- apps/api/v1/router.py
+++ apps/api/v1/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from api.v1.endpoints import user, profile, utility, login, chat, admin
+from api.v1.endpoints import user, profile, utility, login, chat, admin, meeting
 
 api_router = APIRouter()
 
@@ -10,3 +10,4 @@ api_router.include_router(profile.router, tags=["profile"])
 api_router.include_router(utility.router, tags=["utility"])
 api_router.include_router(chat.router, tags=["chat"])
 api_router.include_router(admin.router, tags=["admin"])
+api_router.include_router(meeting.router, tags=["meeting"])
