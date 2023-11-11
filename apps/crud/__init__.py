5f6a685e75f863ecbaf97eee91162dbc172b44c9
Lee Hyun Jun <bhk0827@gmail.com>
2023-11-12 00:40:26 +0900

    <Feat> : Add get_object_or_404()

--- apps/crud/__init__.py
+++ apps/crud/__init__.py
@@ -1,5 +1,17 @@
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+
 from .user import user, verify_password, get_password_hash, send_email
 from .profile import profile, save_upload_file, generate_random_string
 from .utility import utility
 from .chat import chat
 from .meeting import meeting
+
+
+def get_object_or_404(db: Session, model, obj_id: int):
+    obj = db.query(model).filter(model.id == obj_id).first()
+    if not obj:
+        raise HTTPException(
+            status_code=404, detail=f"{model.__name__} with id {obj_id} is not found"
+        )
+    return obj
