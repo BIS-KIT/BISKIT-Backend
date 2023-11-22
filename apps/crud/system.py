c789b92b8693d8a2b170edd7bbacab2a7763a0a2
Lee Hyun Jun <bhk0827@gmail.com>
2023-11-23 01:08:48 +0900

    <Feat> : add service logic

--- apps/crud/system.py
+++ apps/crud/system.py
@@ -1,37 +1,65 @@
-from typing import Any, Optional
+from typing import Any, Optional, List
 from sqlalchemy import func, desc, asc, extract
 from sqlalchemy.orm import Session
 from fastapi import UploadFile, HTTPException
+from models.system import Ban
+from schemas.system import BanCreate
 
 from log import log_error
 from crud.base import CRUDBase
-from models.system import System, Report, Notice
-from schemas.system import (
-    SystemCreate,
-    SystemUpdate,
-    ReportCreate,
-    ReportUpdate,
-    NoticeCreate,
-    NoticeUpdate,
-)
+from models import system as system_models
+from schemas import system as system_schemas
 
 
-class CRUDSystem(CRUDBase[System, SystemCreate, SystemUpdate]):
-    def get_by_user_id(self, db: Session, user_id: int) -> System | None:
-        return db.query(System).filter(System.user_id == user_id).first()
+class CRUDSystem(
+    CRUDBase[
+        system_models.System, system_schemas.SystemCreate, system_schemas.SystemUpdate
+    ]
+):
+    def get_by_user_id(self, db: Session, user_id: int) -> system_models.System | None:
+        return (
+            db.query(system_models.System)
+            .filter(system_models.System.user_id == user_id)
+            .first()
+        )
 
     def create_with_default_value(self, db: Session, user_id: int):
-        obj_in = SystemCreate(user_id=user_id)
+        obj_in = system_schemas.SystemCreate(user_id=user_id)
         return super().create(db=db, obj_in=obj_in)
 
 
-class CRUDReport(CRUDBase[Report, ReportCreate, ReportUpdate]):
+class CRUDReport(
+    CRUDBase[
+        system_models.Report, system_schemas.ReportCreate, system_schemas.ReportUpdate
+    ]
+):
     pass
 
 
-class CRUDNotice(CRUDBase[Notice, NoticeCreate, NoticeUpdate]):
+class CRUDNotice(
+    CRUDBase[
+        system_models.Notice, system_schemas.NoticeCreate, system_schemas.NoticeUpdate
+    ]
+):
     pass
 
 
-system = CRUDSystem(System)
-report = CRUDReport(Report)
+class CRUDBan(
+    CRUDBase[system_models.Ban, system_schemas.BanCreate, system_schemas.BanUpdate]
+):
+    def create(self, db: Session, *, user_id: int, target_id: int) -> Ban:
+        obj_in = BanCreate(reporter_id=user_id, target_id=target_id)
+        return super().create(db=db, obj_in=obj_in)
+
+    def read_ban_user(
+        self, db: Session, user_id: int, skip: int, limit: int
+    ) -> List[Ban]:
+        query = db.query(Ban).filter(Ban.reporter_id == user_id)
+        total_count = query.count()
+        return query.offset(skip).limit(limit).all(), total_count
+
+
+system = CRUDSystem(system_models.System)
+report = CRUDReport(system_models.Report)
+ban = CRUDBan(system_models.Ban)
+notice = CRUDNotice(system_models.Notice)
