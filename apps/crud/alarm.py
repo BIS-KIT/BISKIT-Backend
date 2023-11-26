d634cd7dd95da9c48e73616abbdac0ccabe9832c
Lee Hyun Jun <bhk0827@gmail.com>
2023-11-27 08:54:21 +0900

    <Feat> : fcm alarm need to test

--- apps/crud/alarm.py
+++ apps/crud/alarm.py
@@ -1,10 +1,11 @@
 from firebase_admin import messaging
 from sqlalchemy.orm import Session
+from typing import List
 
 import crud
 
 
-def send_fcm_notification(title, body, fcm_token, data=None):
+def send_fcm_notification(title:str, body:str, fcm_tokens:List[str], data=None):
     """
     FCM을 통해 푸시 알림을 전송하는 함수
     :param title: 알림의 제목
@@ -12,54 +13,57 @@ def send_fcm_notification(title, body, fcm_token, data=None):
     :param fcm_token: 대상 장치의 FCM 토큰
     """
     try:
-        message = messaging.Message(
-            notification=messaging.Notification(title=title, body=body),
-            data=data,
-            token=fcm_token,
-        )
-        response = messaging.send(message)
+        messages = [
+            messaging.Message(
+                notification=messaging.Notification(title=title, body=body),
+                data=data,
+                token=token,
+            ) for token in fcm_tokens
+        ]
+        response = messaging.send_all(messages)
         return f"Successfully sent message: {response}"
     except Exception as e:
         return f"Error sending message: {e}"
 
 
 class Alarm:
-    def create_meeting_request(db: Session, user_id: int, meeting_id: int):
-        fcm_token = crud.user.get_user_fcm_token(db=db, user_id=user_id)
-        data = {"meeting_id": meeting_id}
+    def _get_user_info_and_meeting(self, db: Session, user_id: int, meeting_id: int):
+        fcm_token, nick_name = crud.user.get_user_fcm_token_with_name(db=db, user_id=user_id)
+        meeting = crud.meeting.get(db=db, id=meeting_id)
         if not fcm_token:
-            ValueError("fcm_token is None")
-        res = send_fcm_notification(
-            title="모임 참여 신청", body="모임에 참여 신청이 있습니다.", fcm_token=fcm_token, data=data
-        )
-        return res
+            raise ValueError("fcm_token is None")
+        return fcm_token, nick_name, meeting
+    
+    def create_meeting_request(self, db: Session, user_id: int, meeting_id: int):
+        fcm_token, nick_name, meeting = self._get_user_info_and_meeting(db, user_id, meeting_id)
+        title = "모임 신청"
+        body = f"{nick_name}님이 {meeting.name} 모임에 신청했어요."
+        data = {"meeting_id": meeting_id}
+        return send_fcm_notification(title, body, [fcm_token], data)
 
-    def cancle_meeting(db: Session, user_id: int, meeting_id: int):
-        fcm_token = crud.user.get_user_fcm_token(db=db, user_id=user_id)
+    def exit_meeting(self, db: Session, user_id: int, meeting_id: int):
+        fcm_token, nick_name, meeting = self._get_user_info_and_meeting(db, user_id, meeting_id)
+        title = "모임 취소"
+        body = f"{nick_name}님이 {meeting.name} 모임에서 나갔어요."
         data = {"meeting_id": meeting_id}
-        if not fcm_token:
-            ValueError("fcm_token is None")
-        res = send_fcm_notification(
-            title="모임 취소", body="모임이 취소되었습니다.", fcm_token=fcm_token, data=data
-        )
-        return res
+        return send_fcm_notification(title, body, [fcm_token], data)
 
-    def meeting_request_approve(db: Session, user_id: int, meeting_id: int):
-        fcm_token = crud.user.get_user_fcm_token(db=db, user_id=user_id)
+    def meeting_request_approve(self, db: Session, user_id: int, meeting_id: int):
+        fcm_token, nick_name, meeting = self._get_user_info_and_meeting(db, user_id, meeting_id)
+        title = "모임 승인"
+        body = f"{meeting.name} 모임에 승인되었어요."
         data = {"meeting_id": meeting_id}
-        if not fcm_token:
-            ValueError("fcm_token is None")
-        res = send_fcm_notification(
-            title="모임 참여 신청", body="모임 참여 신청이 승인되었습니다.", fcm_token=fcm_token, data=data
-        )
-        return res
+        return send_fcm_notification(title, body, [fcm_token], data)
 
-    def meeting_request_reject(db: Session, user_id: int, meeting_id: int):
-        fcm_token = crud.user.get_user_fcm_token(db=db, user_id=user_id)
+    def meeting_request_reject(self, db: Session, user_id: int, meeting_id: int):
+        fcm_token, nick_name, meeting = self._get_user_info_and_meeting(db, user_id, meeting_id)
+        title = "모임 거절"
+        body = f"{meeting.name} 모임에 거절되었어요."
         data = {"meeting_id": meeting_id}
-        if not fcm_token:
-            ValueError("fcm_token is None")
-        res = send_fcm_notification(
-            title="모임 참여 신청", body="모임 참여 신청이 거절되었습니다.", fcm_token=fcm_token, data=data
-        )
-        return res
+        return send_fcm_notification(title, body, [fcm_token], data)
+
+
+    def notice_alarm(db:Session, title:str, content:str, notice_id:int):
+        fcm_tokens = crud.user.get_all_fcm_tokens(db=db)  
+        data = {"notice_id": notice_id}
+        return send_fcm_notification(title=title, body=content, fcm_tokens=fcm_tokens, data=data)
