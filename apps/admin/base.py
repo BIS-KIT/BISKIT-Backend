8ac1b11e2cbb0854fdf5b1475ab187d45d416d01
Lee Hyun Jun <bhk0827@gmail.com>
2023-12-03 18:32:43 +0900

    <Feat> : Add Contact in backoffice

--- apps/admin/base.py
+++ apps/admin/base.py
@@ -2,7 +2,7 @@ from sqladmin import ModelView, Admin
 
 from models.user import User
 from models.profile import StudentVerification
-from models.system import Report
+from models.system import Report, Contact
 
 import os
 from pathlib import Path
@@ -17,8 +17,21 @@ templates_dir = f"{folder_path}/admin/templates"
 class UserAdmin(ModelView, model=User):
     column_list = [User.id, User.name]
 
+
+class ContactAdmin(ModelView, model=Contact):
+    column_list = [Contact.id, Contact.user_id, Contact.content]
+
+
 class ReportAdmin(ModelView, model=Report):
-    column_list = [Report.id, Report.content_type, Report.content_id, Report.reason]
+    column_list = [
+        Report.id,
+        Report.content_type,
+        Report.content_id,
+        Report.reason,
+        Report.status,
+    ]
+    list_template = "report_list.html"
+
 
 class StudentVerificationAdmin(ModelView, model=StudentVerification):
     can_edit = False
@@ -34,3 +47,4 @@ def register_all(admin: Admin):
     admin.add_view(UserAdmin)
     admin.add_view(StudentVerificationAdmin)
     admin.add_view(ReportAdmin)
+    admin.add_view(ContactAdmin)
