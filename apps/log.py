2b5c595309d3636512eb69f7d3064c428d79fc52
Lee Hyun Jun <bhk0827@gmail.com>
2023-12-09 23:06:14 +0900

    <Feat> : seperate log for scheduler

--- apps/log.py
+++ apps/log.py
@@ -6,15 +6,20 @@ from logging.handlers import RotatingFileHandler
 current_script_path = os.path.abspath(__file__)
 current_directory = os.path.dirname(current_script_path)
 
-logger = logging.getLogger("biskit")
-
-logger.setLevel(logging.WARNING)
 handler = RotatingFileHandler(
     f"{current_directory}/logging/log", maxBytes=1024 * 1024 * 10, backupCount=3
 )
+
+logger = logging.getLogger("biskit")
+scheduler_logger = logging.getLogger("scheduler")
+
+logger.setLevel(logging.WARNING)
+scheduler_logger.setLevel(logging.WARNING)
+
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+scheduler_logger.addHandler(handler)
 
 
 def log_error(exception: Exception):
