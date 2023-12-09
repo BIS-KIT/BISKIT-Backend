f0830b1b64bd6653d2f4a1aeada667a0195e9da6
Lee Hyun Jun <bhk0827@gmail.com>
2023-12-10 03:40:49 +0900

    <Feat> : seperate logging

--- apps/log.py
+++ apps/log.py
@@ -2,6 +2,8 @@ import logging, os, traceback
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
 
+from schemas.enum import LogTypeEnum
+
 
 current_script_path = os.path.abspath(__file__)
 current_directory = os.path.dirname(current_script_path)
@@ -16,20 +18,36 @@ scheduler_handler = RotatingFileHandler(
     backupCount=1,
 )
 
+alarm_handler = RotatingFileHandler(
+    f"{current_directory}/logging/alarm_log",
+    maxBytes=1024 * 1024 * 10,
+    backupCount=1,
+)
+
 logger = logging.getLogger("biskit")
 scheduler_logger = logging.getLogger("scheduler")
+alarm_logger = logging.getLogger("alarm")
 
 logger.setLevel(logging.WARNING)
 scheduler_logger.setLevel(logging.WARNING)
+alarm_logger.setLevel(logging.WARNING)
 
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 scheduler_handler.setFormatter(formatter)
+alarm_logger.setLevel(logging.WARNING)
+
 
 logger.addHandler(handler)
 scheduler_logger.addHandler(scheduler_handler)
+alarm_logger.addHandler(alarm_handler)
 
 
-def log_error(exception: Exception):
+def log_error(exception: Exception, type: str = LogTypeEnum.DEFAULT.value):
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    logger.error(f"{exception} \n {traceback.format_exc()}")
+    if type == LogTypeEnum.DEFAULT.value:
+        logger.error(f"{exception} \n {traceback.format_exc()}")
+    elif type == LogTypeEnum.SCHEDULER.value:
+        scheduler_logger.warning(f"{exception} \n {traceback.format_exc()}")
+    elif type == LogTypeEnum.ALARM.value:
+        alarm_logger.warning(f"{exception} \n {traceback.format_exc()}")
