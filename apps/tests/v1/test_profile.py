53cd2e38d979a0a9c714a0bfa740d4051f741745
Lee Hyun Jun <bhk0827@gmail.com>
2024-02-22 21:07:49 +0900

    <Feat> : test for ordery_by, status

--- apps/tests/v1/test_profile.py
+++ apps/tests/v1/test_profile.py
@@ -1,5 +1,7 @@
 import json
 import pytest
+from datetime import datetime, timedelta
+
 from tests.confest import (
     client,
     session,
@@ -9,6 +11,11 @@ from tests.confest import (
     test_token,
     test_profile,
     test_language,
+    test_tag,
+    test_topic,
+    create_test_meeting,
+    create_test_meeting_user,
+    create_test_user,
 )
 
 from schemas.enum import ReultStatusEnum, LanguageLevelEnum
@@ -16,7 +23,6 @@ from schemas import profile as profile_schmea
 from models import profile as profile_models
 
 
-@pytest.mark.skip
 def test_get_profile_photos(
     client,
     test_user,
@@ -41,7 +47,6 @@ def test_get_profile_photos(
     assert "nationalities" in data[0]
 
 
-@pytest.mark.skip
 def test_check_nick_name(client, test_profile):
     nick_name = test_profile.nick_name
 
@@ -50,7 +55,6 @@ def test_check_nick_name(client, test_profile):
     assert response.status_code != 200
 
 
-@pytest.mark.skip
 def test_student_varification(client, test_profile):
     user_id = test_profile.user_id
     student_card = "test_student_card"
@@ -67,7 +71,6 @@ def test_student_varification(client, test_profile):
     assert ReultStatusEnum.PENDING.value == data.get("verification_status")
 
 
-@pytest.mark.skip
 def test_read_student_varification(client, test_profile):
     user_id = test_profile.user_id
     test_student_card = test_profile.student_verification
@@ -82,7 +85,6 @@ def test_read_student_varification(client, test_profile):
     assert data.get("verification_status") == test_student_card.verification_status
 
 
-@pytest.mark.skip
 def test_delete_profile_photo(client, test_profile):
     user_id = test_profile.user_id
 
@@ -93,7 +95,6 @@ def test_delete_profile_photo(client, test_profile):
     assert test_profile.profile_photo == None
 
 
-@pytest.mark.skip
 def test_create_profile(client, test_user, test_language):
     user_id = test_user.id
     test_language_confest = test_language
@@ -102,13 +103,13 @@ def test_create_profile(client, test_user, test_language):
         level="BASIC", language_id=test_language_confest.id
     )
 
-    profile_schmea = profile_schmea.ProfileRegister(
+    profile_schema_obj = profile_schmea.ProfileRegister(
         nick_name="test_profile",
         profile_photo="test_profile_photo",
         available_languages=[language_schema],
     )
 
-    json_data = json.loads(profile_schmea.model_dump_json())
+    json_data = json.loads(profile_schema_obj.model_dump_json())
 
     # HTTP 요청 시뮬레이션
     response = client.post(f"/v1/profile?user_id={user_id}", json=json_data)
@@ -153,7 +154,126 @@ def test_update_profile(client, test_profile):
     assert data["user_university"]["education_status"] == "졸업"
 
 
-def test_get_user_meetings(client, test_user):
+def test_get_user_meetings_with_order_by(
+    session, client, test_user, test_topic, test_tag, test_language
+):
     user_fixture = test_user
 
-    response = client.get(f"v1/profile/{user_fixture.id}/meetings")
+    now = datetime.now()
+    test_meeting1 = create_test_meeting(
+        session=session,
+        user_id=user_fixture.id,
+        test_topic=test_topic,
+        test_tag=test_tag,
+        test_language=test_language,
+        meeting_time=now + timedelta(days=2),
+    )
+
+    test_meeting2 = create_test_meeting(
+        session=session,
+        user_id=user_fixture.id,
+        test_topic=test_topic,
+        test_tag=test_tag,
+        test_language=test_language,
+        meeting_time=now + timedelta(days=1),
+    )
+
+    test_meeting3 = create_test_meeting(
+        session=session,
+        user_id=user_fixture.id,
+        test_topic=test_topic,
+        test_tag=test_tag,
+        test_language=test_language,
+        meeting_time=now,
+    )
+
+    response = client.get(
+        f"v1/profile/{user_fixture.id}/meetings?order_by=meeting_time"
+    )
+
+    assert response.status_code == 200, response.content
+
+    data = response.json()
+
+    assert data["total_count"] == 3
+
+    # test order_by filter
+    assert (
+        data["meetings"][0]["id"] == test_meeting3["id"]
+    ), "Meetings are not in the expected order"
+    assert (
+        data["meetings"][1]["id"] == test_meeting2["id"]
+    ), "Meetings are not in the expected order"
+    assert (
+        data["meetings"][2]["id"] == test_meeting1["id"]
+    ), "Meetings are not in the expected order"
+
+
+def test_get_user_meetings_with_status(
+    session,
+    client,
+    test_user,
+    test_topic,
+    test_tag,
+    test_language,
+    test_nationality,
+    test_university,
+):
+    user_fixture = test_user
+
+    now = datetime.now()
+    pending_test_meeting = create_test_meeting(
+        session=session,
+        user_id=user_fixture.id,
+        test_topic=test_topic,
+        test_tag=test_tag,
+        test_language=test_language,
+        meeting_time=now,
+    )
+
+    approve_test_meeting = create_test_meeting(
+        session=session,
+        user_id=user_fixture.id,
+        test_topic=test_topic,
+        test_tag=test_tag,
+        test_language=test_language,
+        meeting_time=now,
+    )
+    # test status filter
+    status_test_user = create_test_user(
+        session=session,
+        test_nationality=test_nationality,
+        test_university=test_university,
+    )
+    pending_meeting_user = create_test_meeting_user(
+        session=session,
+        user_id=status_test_user["id"],
+        meeting_id=pending_test_meeting["id"],
+    )
+
+    approve_meeting_user = create_test_meeting_user(
+        session=session,
+        user_id=status_test_user["id"],
+        meeting_id=approve_test_meeting["id"],
+        is_approve=True,
+    )
+
+    pending_response = client.get(
+        f"v1/profile/{status_test_user['id']}/meetings?status=PENDING"
+    )
+
+    assert pending_response.status_code == 200, pending_response.content
+
+    pending_data = pending_response.json()
+
+    assert pending_data["meetings"][0]["id"] == pending_test_meeting["id"]
+
+    approve_response = client.get(
+        f"v1/profile/{status_test_user['id']}/meetings?status=APPROVE"
+    )
+
+    assert approve_response.status_code == 200, approve_response.content
+
+    approve_data = approve_response.json()
+
+    assert approve_data["meetings"][0]["id"] == approve_test_meeting["id"]
