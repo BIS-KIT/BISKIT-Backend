c4f0bce52e142f5e2f9276a8b81da7112710d413
Lee Hyun Jun <bhk0827@gmail.com>
2024-03-03 17:59:14 +0900

    <Feat> : add ios user fixture

--- apps/tests/fixtures/user_fixture.py
+++ apps/tests/fixtures/user_fixture.py
@@ -22,7 +22,55 @@ def test_user(session, test_nationality, test_university):
         name="이현준",
         birth="2000-12-13",
         gender="male",
-        fcm_token="e5k60HCtTo-NPXw1L1OIGe:APA91bEvp9T5wTAjTEq5yEQFaExjbA8LpkKr_C92t-5_XlGvWQh4cVKSxQYb6ybysMlbwd9hV-RMCyoR_VfjS1gqxV0eIPI0Pzcd_ukYGAmvEPuyETU8NXvKBeE_urAxKBHCOpsPLWz8",
+        fcm_token="cSQIeWS6T66K7cgqfcj7Ms:APA91bFo7WnZI29iDPAM3wBYHj7Pyp2lUTWr7BtHSvYs1fOgp3SQv_LKCEwxgSsVDhwDj1wkbnIUPWFbmEGzZvtwfIWehIZUWiAqSywTE0KhIMLjt0wnJ4brRrA5F3xQde1yClpxLFuC",
+    )
+    session.add(user)
+    session.flush()
+
+    # Consent 객체 생성 및 연결
+    consent = user_models.Consent(
+        user_id=user.id, terms_mandatory=True, terms_optional=False, terms_push=False
+    )
+    session.add(consent)
+
+    # UserNationality 객체 생성 및 연결
+    user_nationality1 = user_models.UserNationality(
+        user_id=user.id, nationality_id=nationality1.id
+    )
+    user_nationality2 = user_models.UserNationality(
+        user_id=user.id, nationality_id=nationality2.id
+    )
+    session.add(user_nationality1)
+    session.add(user_nationality2)
+
+    user_university = profile_models.UserUniversity(
+        department="test_department",
+        education_status="test_status",
+        university_id=test_university_fixture.id,
+        user_id=user.id,
+    )
+
+    session.add(user_university)
+
+    session.commit()
+
+    return user
+
+
+@pytest.fixture(scope="function")
+def test_user_ios(session, test_nationality, test_university):
+    nationality1, nationality2 = test_nationality
+    test_university_fixture = test_university
+
+    user = user_models.User(
+        email="test_ios@gmail.com",
+        password=get_password_hash("guswns95@@"),
+        sns_type="kakao",
+        sns_id="testsnslogin",
+        name="김유나",
+        birth="2000-08-04",
+        gender="female",
+        fcm_token="eoKqrEfH3E8dnxN9XaTwRj:APA91bGmgnbDCvH7ZKWM51IZ0pDI2JXUS7z3o9qFyQLEILTyVCgiMPE2BED_fUQiQxM85FcW-xHilSUaJ4oD72gOg5zmHpYAH0smajwXAdwTCV8g4j71ifQE77I-KffqhHg5qAjE3yFN",
     )
     session.add(user)
     session.flush()
