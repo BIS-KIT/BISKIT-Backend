3945261193f2b181845aaf195555de540e895f4f
Lee Hyun Jun <bhk0827@gmail.com>
2024-03-03 17:58:19 +0900

    <Feat> : add Alarm fixture

--- apps/tests/confest.py
+++ apps/tests/confest.py
@@ -5,3 +5,4 @@ from .fixtures.utility_fixture import *
 from .fixtures.utils import *
 from .fixtures.meeting_fixture import *
 from .fixtures.system_fixture import *
+from .fixtures.alarm_fixture import *
