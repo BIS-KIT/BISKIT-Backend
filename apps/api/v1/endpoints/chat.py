79c71b4023900448384a03115df503506628353b
JEONGMUN LEE <lot9616@gmail.com>
2024-03-29 21:17:06 +0900

    [Fix] chat auth

--- apps/api/v1/endpoints/chat.py
+++ apps/api/v1/endpoints/chat.py
@@ -1,24 +1,32 @@
-from typing import Any, List, Optional, Dict
+from typing import Any, List, Optional, Dict, Annotated
 
-from fastapi import APIRouter, Body, Depends, UploadFile,HTTPException
+from fastapi import APIRouter, Body, Depends, UploadFile, HTTPException
 from sqlalchemy.orm import Session
 
 import crud
+from core.security import oauth2_scheme
 from database.session import get_db
 from schemas.chat import CahtImageCreate, ChatImageResponse
 from log import log_error
 
 router = APIRouter()
 
-@router.post("/chat/upload",response_model=ChatImageResponse)
+
+@router.post("/chat/upload", response_model=ChatImageResponse)
 def upload_file_chat(
-    photo: UploadFile, chat_id:str, message_id:str,db: Session = Depends(get_db)
+    photo: UploadFile,
+    chat_id: str,
+    message_id: str,
+    db: Session = Depends(get_db),
+    token: Annotated[str, Depends(oauth2_scheme)] = None,
 ):
     return_obj = None
     file_path = f"chat_file/{chat_id}/{photo.filename}"
     try:
         image_url = crud.save_upload_file(upload_file=photo, destination=file_path)
-        chat_image = CahtImageCreate(image_url=image_url, chat_id=chat_id, message_id=message_id)
+        chat_image = CahtImageCreate(
+            image_url=image_url, chat_id=chat_id, message_id=message_id
+        )
         return_obj = crud.chat.create(db=db, obj_in=chat_image)
     except Exception as e:  # 어떤 예외든지 캐치합니다.
         log_error(e)
