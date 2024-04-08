aaa3d39a1d948c44e8efa83ca4651dad585fa441
Lee Hyun Jun <bhk0827@gmail.com>
2024-04-09 00:00:04 +0900

    <Feat> : Add sql logger

--- apps/log.py
+++ apps/log.py
@@ -24,23 +24,32 @@ alarm_handler = RotatingFileHandler(
     backupCount=1,
 )
 
+sql_handler = RotatingFileHandler(
+    f"{current_directory}/logging/sql_log",
+    maxBytes=1024 * 1024 * 10,
+    backupCount=1,
+)
+
 logger = logging.getLogger("biskit")
 scheduler_logger = logging.getLogger("scheduler")
 alarm_logger = logging.getLogger("alarm")
+sql_logger = logging.getLogger("sqlalchemy.engine")
 
 logger.setLevel(logging.WARNING)
 scheduler_logger.setLevel(logging.WARNING)
 alarm_logger.setLevel(logging.WARNING)
+sql_logger.setLevel(logging.INFO)
+
 
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 scheduler_handler.setFormatter(formatter)
-alarm_logger.setLevel(logging.WARNING)
 
 
 logger.addHandler(handler)
 scheduler_logger.addHandler(scheduler_handler)
 alarm_logger.addHandler(alarm_handler)
+sql_logger.addHandler(sql_handler)
 
 
 def log_error(exception: Exception, type: str = LogTypeEnum.DEFAULT.value):
