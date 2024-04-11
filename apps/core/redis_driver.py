ce8c43b450cd69f0d8fe80eb2698886d18d5b73f
LHJ <bhk0827@gmail.com>
2024-04-11 07:51:58 +0000

    <Feat> : add name space in key & find method

--- apps/core/redis_driver.py
+++ apps/core/redis_driver.py
@@ -1,6 +1,9 @@
 import redis
 import hashlib
 import json
+from typing import List
+
+
 from core.config import settings
 
 
@@ -8,11 +11,11 @@ class RedisDriver:
     def __init__(self, redis_url: str):
         self.redis_url = redis_url
         self.redis_client = None
-        self.default_expire_time = 600
+        self.default_expire_time = 60
 
-    def generate_cache_key(self, *args, **kwargs) -> str:
+    def generate_cache_key(self, name_space: str, *args, **kwargs) -> str:
         """
-        주어진 파라미터들로부터 유니크한 캐시 키를 생성합니다. 파라미터들은 해시 함수를 통해 처리됩니다.
+        주어진 파라미터와 name_space로 유니크한 캐시 키를 생성
         """
         # args와 kwargs를 정렬된 상태로 문자열로 변환
         key_base = json.dumps(
@@ -20,7 +23,7 @@ class RedisDriver:
         )
         # MD5 해시 함수를 사용하여 유니크한 키 생성
         key_hash = hashlib.md5(key_base.encode()).hexdigest()
-        return f"cache:{key_hash}"
+        return f"{name_space}:{key_hash}"
 
     def connect(self):
         self.redis_client = redis.Redis.from_url(self.redis_url)
@@ -45,6 +48,20 @@ class RedisDriver:
         주어진 키가 Redis에 존재하는지 확인합니다.
         """
         return self.redis_client.exists(key) > 0
+
+    def find_by_name_space(self, name_space: str):
+        """
+        name_space로 캐시 키 검색
+        """
+        pattern = f"{name_space}:*"
+        return [key for key in self.redis_client.scan_iter(match=pattern)]
+
+    def delete_keys(self, key_list: List):
+        """
+        캐시 삭제
+        """
+        [self.redis_client.delete(key) for key in key_list]
+        return True
 
 
 redis_driver = RedisDriver(redis_url=f"redis://{settings.REDIS_HOST}")
