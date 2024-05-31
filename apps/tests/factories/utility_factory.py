6d5b471507855459415162dc8f079b75d7cf7f3e
Lee Hyun Jun <bhk0827@gmail.com>
2024-06-01 08:04:25 +0900

    <Feat> : Complete utility factory

--- apps/tests/factories/utility_factory.py
+++ apps/tests/factories/utility_factory.py
@@ -6,11 +6,18 @@ from models import utility as utility_models
 
 class NationalityFactory(BaseFactory):
 
+    kr_name = factory.Iterator(["대한민국", "미국"])
+    en_name = factory.Iterator(["Korea", "USA"])
+    code = factory.Iterator(["kr", "usa"])
+
     class Meta:
         model = utility_models.Nationality
 
 
 class LanguageFactory(BaseFactory):
+
+    kr_name = factory.Iterator(["한국어", "영어"])
+    en_name = factory.Iterator(["Korean", "English"])
 
     class Meta:
         model = utility_models.Language
@@ -18,17 +25,28 @@ class LanguageFactory(BaseFactory):
 
 class UniversityFactory(BaseFactory):
 
+    kr_name = factory.Iterator(["서울대학교", "고려대학교"])
+    en_name = factory.Faker("name")
+    campus_type = factory.Faker("word")
+    location = factory.Faker("address")
+
     class Meta:
         model = utility_models.University
 
 
 class TagFactory(BaseFactory):
 
+    kr_name = factory.Faker("word")
+    en_name = factory.Faker("word")
+
     class Meta:
         model = utility_models.Tag
 
 
 class TopicFactory(BaseFactory):
+
+    kr_name = factory.Faker("word")
+    en_name = factory.Faker("word")
 
     class Meta:
         model = utility_models.Topic
