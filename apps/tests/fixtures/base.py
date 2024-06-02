39e47059256359b6741e70e4f1e03d0c78ae57fa
Lee Hyun Jun <bhk0827@gmail.com>
2024-06-03 01:58:42 +0900

    <Feat> : clearing test db session

--- apps/tests/fixtures/base.py
+++ apps/tests/fixtures/base.py
@@ -8,7 +8,7 @@ from sqlalchemy_utils import database_exists, create_database
 
 from main import create_app
 from core.config import settings
-from database.session import Base, get_db
+from database.session import get_db
 from models.base import ModelBase
 
 
@@ -18,49 +18,43 @@ engine = create_engine(
     DATABASE_URL,
     poolclass=StaticPool,
 )
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, expire_on_commit=False, bind=engine
+)
+
+ModelBase.metadata.drop_all(bind=engine)
+ModelBase.metadata.create_all(bind=engine)
 
 if not database_exists(engine.url):
     create_database(engine.url)
 
-ModelBase.metadata.create_all(bind=engine)
 
 test_app = create_app()
 
 
-@pytest.fixture(scope="function")
-def session():
-    ModelBase.metadata.drop_all(bind=engine)
-    ModelBase.metadata.create_all(bind=engine)
+def get_test_token():
+    from core.security import create_access_token
+    from ..factories import UserFactory
 
+    test_user = UserFactory()
+
+    login_data = {"username": test_user.email, "auth_method": "email"}
+
+    access_token = create_access_token(data=login_data)
+    return access_token
+
+
+@pytest.fixture(scope="session")
+def client():
     db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
-
-@pytest.fixture(scope="function")
-def client(session):
     def override_get_db():
         try:
-            yield session
+            yield db
         finally:
-            session.close()
+            db.close()
 
-    # app에서 사용하는 DB를 오버라이드하는 부분
     test_app.dependency_overrides[get_db] = override_get_db
-
-    def get_test_token():
-        with TestClient(test_app) as client:
-            login_data = {
-                "username": "test@example.com",
-                "password": "test_password",
-            }
-            response = client.post("v1/token", data=login_data)
-            tokens = response.json()
-            access_token = tokens["access_token"]
-            return access_token
 
     # 테스트 클라이언트 생성
     test_client = TestClient(test_app)
