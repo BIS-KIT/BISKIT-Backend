ff9abd2685d53008785242b0beceae9213eab1d3
Lee Hyun Jun <bhk0827@gmail.com>
2024-06-03 05:28:02 +0900

    <Feat> : fixture & test for profile

--- apps/tests/factories/profile_factory.py
+++ apps/tests/factories/profile_factory.py
@@ -9,55 +9,82 @@ from models import profile as profile_models
 
 class ProfileFactory(BaseFactory):
 
-    class Meta:
-        model = profile_models.Profile
-
     profile_photo = factory.Faker("image_url")
     nick_name = factory.Faker("name")
     context = factory.Faker("text")
     is_default_photo = False
     user = factory.SubFactory(UserFactory)
 
+    class Meta:
+        model = profile_models.Profile
+
+    class Params:
+        with_university = factory.Trait(
+            user_university=factory.RelatedFactory(
+                "tests.factories.profile_factory.UserUniversityFactory", "profile"
+            )
+        )
+
+        with_language = factory.Trait(
+            available_language_list=factory.RelatedFactoryList(
+                "tests.factories.profile_factory.AvailableLanguageFactory",
+                "profile",
+                size=2,
+            )
+        )
+
+        with_student_card = factory.Trait(
+            student_verification=factory.RelatedFactory(
+                "tests.factories.profile_factory.StudentVerificationFactory", "profile"
+            )
+        )
+
+        with_introduction = factory.Trait(
+            introductions=factory.RelatedFactoryList(
+                "tests.factories.profile_factory.IntroductionFactory", "profile", size=2
+            )
+        )
+
 
 class UserUniversityFactory(BaseFactory):
-
-    class Meta:
-        model = profile_models.UserUniversity
 
     department = factory.Iterator(
         ["UNDERGRADUATE", "GRADUATE", "EXCHANGE_STUDENT", "LANGUAGE_INSTITUTE"]
     )
     education_status = factory.Iterator(["undergraduate", "graduate"])
-    university = factory.RelatedFactoryList(LanguageFactory, size=2)
-    user = factory.SubFactory(UserFactory)
+    university = factory.RelatedFactory(UniversityFactory)
+    user_id = factory.LazyAttribute(lambda x: x.profile.user_id)
     profile = factory.SubFactory(ProfileFactory)
+
+    class Meta:
+        model = profile_models.UserUniversity
 
 
 class AvailableLanguageFactory(BaseFactory):
 
-    class Meta:
-        model = profile_models.AvailableLanguage
-
-    level = factory.Iterator(["beginner", "intermediate", "advanced"])
+    level = factory.Iterator(["BEGINNER", "INTERMEDIATE", "ADVANCED"])
     language = factory.SubFactory(LanguageFactory)
     profile = factory.SubFactory(ProfileFactory)
 
+    class Meta:
+        model = profile_models.AvailableLanguage
+
 
 class IntroductionFactory(BaseFactory):
-
-    class Meta:
-        model = profile_models.Introduction
 
     keyword = factory.Faker("word")
     context = factory.Faker("text")
     profile = factory.SubFactory(ProfileFactory)
 
+    class Meta:
+        model = profile_models.Introduction
+
 
 class StudentVerificationFactory(BaseFactory):
-
-    class Meta:
-        model = profile_models.StudentVerification
 
     student_card = factory.Faker("uuid4")
     verification_status = factory.Iterator(["pending", "approved", "rejected"])
     profile = factory.SubFactory(ProfileFactory)
+
+    class Meta:
+        model = profile_models.StudentVerification
