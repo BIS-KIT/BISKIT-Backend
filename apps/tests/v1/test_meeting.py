b3f9b6eb48631cdd14d5da839b44be48f482ba6c
Lee Hyun Jun <bhk0827@gmail.com>
2024-06-08 23:00:38 +0900

    <WIP> : Mocking

--- apps/tests/v1/test_meeting.py
+++ apps/tests/v1/test_meeting.py
@@ -3,6 +3,8 @@ import pytest
 from datetime import datetime, timedelta
 
 from tests.confest import *
+from unittest.mock import patch, MagicMock
+from core.redis_driver import redis_driver
 from schemas import meeting as meeting_schmea
 from schemas.enum import ReultStatusEnum
 
@@ -28,7 +30,13 @@ def test_create_meeting(client, test_user, test_tag, test_topic, test_language):
 
     json_data = json.loads(meeting_create_schema.model_dump_json())
 
-    response = client.post("v1/meeting", json=json_data)
+    # RedisDriver Mocking
+    mock_redis_driver = MagicMock(spec=redis_driver)
+    mock_redis_driver.find_by_name_space.return_value = []
+    mock_redis_driver.delete_keys.return_value = True
+
+    with patch("crud.meeting.redis_driver", new=mock_redis_driver):
+        response = client.post("v1/meeting", json=json_data)
 
     assert response.status_code == 200, response.content
 
@@ -41,265 +49,264 @@ def test_create_meeting(client, test_user, test_tag, test_topic, test_language):
     assert data["foreign_count"] == 0
 
 
-def test_get_meeting_requests(client, session, test_meeting, test_user):
-    test_join_request = create_test_join_request(
-        session=session, user_id=test_user.id, meeting_id=test_meeting.id
-    )
+# def test_get_meeting_requests(
+#     client, session, test_meeting_with_participants, test_user
+# ):
 
-    response = client.get(f"v1/meetings/{test_meeting.id}/requests")
+#     response = client.get(f"v1/meetings/{test_meeting_with_participants.id}/requests")
 
-    assert response.status_code == 200, response.content
+#     assert response.status_code == 200, response.content
 
-    data = response.json()
+#     data = response.json()
+#     print(data)
+#     assert data["total_count"] == 1
 
-    assert data["total_count"] == 1
 
+# def test_check_meeting_request_status(client, session, test_user, test_meeting):
+#     test_join_request = create_test_join_request(
+#         session=session, user_id=test_user.id, meeting_id=test_meeting.id
+#     )
 
-def test_check_meeting_request_status(client, session, test_user, test_meeting):
-    test_join_request = create_test_join_request(
-        session=session, user_id=test_user.id, meeting_id=test_meeting.id
-    )
+#     response = client.get(f"v1/meeting/{test_meeting.id}/user/{test_user.id}")
 
-    response = client.get(f"v1/meeting/{test_meeting.id}/user/{test_user.id}")
+#     assert response.status_code == 200, response.content
 
-    assert response.status_code == 200, response.content
+#     data = response.json()
 
-    data = response.json()
+#     assert data["status"] == ReultStatusEnum.PENDING
 
-    assert data["status"] == ReultStatusEnum.PENDING
 
+# def test_join_meeting_request(
+#     session, client, test_meeting, test_nationality, test_university, test_language
+# ):
+#     test_user = create_test_user(
+#         session=session,
+#         test_nationality=test_nationality,
+#         test_university=test_university,
+#         test_language=test_language,
+#     )
 
-def test_join_meeting_request(
-    session, client, test_meeting, test_nationality, test_university, test_language
-):
-    test_user = create_test_user(
-        session=session,
-        test_nationality=test_nationality,
-        test_university=test_university,
-        test_language=test_language,
-    )
+#     join_requst_schema = meeting_schmea.MeetingUserCreate(
+#         meeting_id=test_meeting.id, user_id=test_user["id"]
+#     )
 
-    join_requst_schema = meeting_schmea.MeetingUserCreate(
-        meeting_id=test_meeting.id, user_id=test_user["id"]
-    )
+#     json_data = json.loads(join_requst_schema.model_dump_json())
 
-    json_data = json.loads(join_requst_schema.model_dump_json())
+#     response = client.post("v1/meeting/join/request", json=json_data)
 
-    response = client.post("v1/meeting/join/request", json=json_data)
+#     assert response.status_code == 200, response.content
+
+
+# def test_join_meeting_approve(
+#     session, client, test_meeting, test_nationality, test_university, test_language
+# ):
+#     test_user = create_test_user(
+#         session=session,
+#         test_nationality=test_nationality,
+#         test_university=test_university,
+#         test_language=test_language,
+#     )
 
-    assert response.status_code == 200, response.content
+#     meeting_request = create_test_meeting_user(
+#         session=session, user_id=test_user["id"], meeting_id=test_meeting.id
+#     )
 
+#     response = client.post(f"/v1/meeting/join/approve?obj_id={meeting_request['id']}")
 
-def test_join_meeting_approve(
-    session, client, test_meeting, test_nationality, test_university, test_language
-):
-    test_user = create_test_user(
-        session=session,
-        test_nationality=test_nationality,
-        test_university=test_university,
-        test_language=test_language,
-    )
+#     assert response.status_code == 201, response.content
 
-    meeting_request = create_test_meeting_user(
-        session=session, user_id=test_user["id"], meeting_id=test_meeting.id
-    )
+#     data = response.json()
 
-    response = client.post(f"/v1/meeting/join/approve?obj_id={meeting_request['id']}")
+#     assert data["status"] == ReultStatusEnum.APPROVE
 
-    assert response.status_code == 201, response.content
 
-    data = response.json()
+# def test_exit_meeting(
+#     session, client, test_meeting, test_language, test_nationality, test_university
+# ):
+#     test_user = create_test_user(
+#         session=session,
+#         test_language=test_language,
+#         test_nationality=test_nationality,
+#         test_university=test_university,
+#         name="test_user",
+#     )
 
-    assert data["status"] == ReultStatusEnum.APPROVE
+#     meeting_request = create_test_meeting_user(
+#         session=session,
+#         user_id=test_user["id"],
+#         meeting_id=test_meeting.id,
+#         is_approve=True,
+#     )
 
+#     response = client.post(f"v1/meeting/{test_meeting.id}/user/{test_user['id']}/exit")
 
-def test_exit_meeting(
-    session, client, test_meeting, test_language, test_nationality, test_university
-):
-    test_user = create_test_user(
-        session=session,
-        test_language=test_language,
-        test_nationality=test_nationality,
-        test_university=test_university,
-        name="test_user",
-    )
+#     assert response.status_code == 200, response.content
 
-    meeting_request = create_test_meeting_user(
-        session=session,
-        user_id=test_user["id"],
-        meeting_id=test_meeting.id,
-        is_approve=True,
-    )
 
-    response = client.post(f"v1/meeting/{test_meeting.id}/user/{test_user['id']}/exit")
+# def test_join_meeting_reject(
+#     session, client, test_meeting, test_nationality, test_university, test_language
+# ):
+#     test_user = create_test_user(
+#         session=session,
+#         test_nationality=test_nationality,
+#         test_university=test_university,
+#         test_language=test_language,
+#     )
 
-    assert response.status_code == 200, response.content
+#     meeting_request = create_test_meeting_user(
+#         session=session, user_id=test_user["id"], meeting_id=test_meeting.id
+#     )
 
+#     response = client.post(f"/v1/meeting/join/reject?obj_id={meeting_request['id']}")
 
-def test_join_meeting_reject(
-    session, client, test_meeting, test_nationality, test_university, test_language
-):
-    test_user = create_test_user(
-        session=session,
-        test_nationality=test_nationality,
-        test_university=test_university,
-        test_language=test_language,
-    )
+#     assert response.status_code == 200, response.content
 
-    meeting_request = create_test_meeting_user(
-        session=session, user_id=test_user["id"], meeting_id=test_meeting.id
-    )
 
-    response = client.post(f"/v1/meeting/join/reject?obj_id={meeting_request['id']}")
+# def test_get_meetings_by_user_university(
+#     session,
+#     client,
+#     test_user,
+#     test_topic,
+#     test_tag,
+#     test_language,
+#     test_university,
+#     test_nationality,
+# ):
 
-    assert response.status_code == 200, response.content
+#     test_meeting = create_test_meeting(
+#         session=session,
+#         user_id=test_user.id,
+#         university_id=test_university.id,
+#         test_tag=test_tag,
+#         test_topic=test_topic,
+#         test_language=test_language,
+#     )
 
+#     test_user_for_meeting = create_test_user(
+#         session=session,
+#         test_nationality=test_nationality,
+#         test_university=test_university,
+#         test_language=test_language,
+#         name="test_usear",
+#     )
+#     test_meeting2 = create_test_meeting(
+#         session=session,
+#         user_id=test_user_for_meeting["id"],
+#         university_id=test_university.id,
+#         test_tag=test_tag,
+#         test_topic=test_topic,
+#         test_language=test_language,
+#     )
 
-def test_get_meetings_by_user_university(
-    session,
-    client,
-    test_user,
-    test_topic,
-    test_tag,
-    test_language,
-    test_university,
-    test_nationality,
-):
+#     response = client.get(f"v1/meeting/{test_user.id}/universty")
 
-    test_meeting = create_test_meeting(
-        session=session,
-        user_id=test_user.id,
-        university_id=test_university.id,
-        test_tag=test_tag,
-        test_topic=test_topic,
-        test_language=test_language,
-    )
+#     assert response.status_code == 200, response.content
 
-    test_user_for_meeting = create_test_user(
-        session=session,
-        test_nationality=test_nationality,
-        test_university=test_university,
-        test_language=test_language,
-        name="test_usear",
-    )
-    test_meeting2 = create_test_meeting(
-        session=session,
-        user_id=test_user_for_meeting["id"],
-        university_id=test_university.id,
-        test_tag=test_tag,
-        test_topic=test_topic,
-        test_language=test_language,
-    )
+#     data = response.json()
 
-    response = client.get(f"v1/meeting/{test_user.id}/universty")
+#     assert data["total_count"] == 2
 
-    assert response.status_code == 200, response.content
 
-    data = response.json()
+# def test_get_meetings_detail(client, test_meeting):
 
-    assert data["total_count"] == 2
+#     response = client.get(f"v1/meeting/{test_meeting.id}")
 
+#     assert response.status_code == 200, response.content
 
-def test_get_meetings_detail(client, test_meeting):
+#     data = response.json()
 
-    response = client.get(f"v1/meeting/{test_meeting.id}")
+#     assert data["id"] == test_meeting.id
 
-    assert response.status_code == 200, response.content
 
-    data = response.json()
+# def test_delete_meeting(client, test_meeting):
 
-    assert data["id"] == test_meeting.id
+#     response = client.delete(f"v1/meeting/{test_meeting.id}")
 
+#     assert response.status_code == 204, response.content
 
-def test_delete_meeting(client, test_meeting):
 
-    response = client.delete(f"v1/meeting/{test_meeting.id}")
+# def test_update_meeting(client, test_meeting):
+#     update_name = "update_test_meeting"
+#     update_participants = test_meeting.max_participants + 1
 
-    assert response.status_code == 204, response.content
+#     update_schmea = meeting_schmea.MeetingUpdate(
+#         name=update_name, max_participants=update_participants
+#     )
 
+#     json_data = json.loads(update_schmea.model_dump_json(exclude_unset=True))
 
-def test_update_meeting(client, test_meeting):
-    update_name = "update_test_meeting"
-    update_participants = test_meeting.max_participants + 1
+#     response = client.put(f"v1/meeting/{test_meeting.id}", json=json_data)
 
-    update_schmea = meeting_schmea.MeetingUpdate(
-        name=update_name, max_participants=update_participants
-    )
+#     assert response.status_code == 200, response.content
 
-    json_data = json.loads(update_schmea.model_dump_json(exclude_unset=True))
+#     data = response.json()
 
-    response = client.put(f"v1/meeting/{test_meeting.id}", json=json_data)
+#     assert data["name"] == update_name
+#     assert data["max_participants"] == update_participants
 
-    assert response.status_code == 200, response.content
 
-    data = response.json()
+# def test_get_meeting_with_filter():
+#     pass
 
-    assert data["name"] == update_name
-    assert data["max_participants"] == update_participants
 
+# def test_get_meeting_all_review(session, client, test_user, test_meeting):
+#     test_review = create_test_review(
+#         session=session, meeting_id=test_meeting.id, user_id=test_user.id
+#     )
 
-def test_get_meeting_with_filter():
-    pass
+#     response = client.get(f"v1/meeting/reviews/{test_user.id}")
 
+#     assert response.status_code == 200, response.content
 
-def test_get_meeting_all_review(session, client, test_user, test_meeting):
-    test_review = create_test_review(
-        session=session, meeting_id=test_meeting.id, user_id=test_user.id
-    )
+#     data = response.json()
 
-    response = client.get(f"v1/meeting/reviews/{test_user.id}")
+#     assert data["reviews"][0]["context"] == test_review["context"]
 
-    assert response.status_code == 200, response.content
 
-    data = response.json()
+# def test_create_review(client, test_user, test_meeting):
+#     test_context = "test_context"
+#     meeting_id = test_meeting.id
+#     review_schema = meeting_schmea.ReviewIn(
+#         context=test_context, creator_id=test_user.id
+#     )
 
-    assert data["reviews"][0]["context"] == test_review["context"]
+#     json_data = json.loads(review_schema.model_dump_json(exclude_unset=True))
 
+#     response = client.post(f"v1/meeting/{meeting_id}/reviews", json=json_data)
 
-def test_create_review(client, test_user, test_meeting):
-    test_context = "test_context"
-    meeting_id = test_meeting.id
-    review_schema = meeting_schmea.ReviewIn(
-        context=test_context, creator_id=test_user.id
-    )
+#     assert response.status_code == 200, response.content
 
-    json_data = json.loads(review_schema.model_dump_json(exclude_unset=True))
+#     data = response.json()
 
-    response = client.post(f"v1/meeting/{meeting_id}/reviews", json=json_data)
+#     assert data["meeting_id"] == meeting_id
+#     assert data["context"] == test_context
 
-    assert response.status_code == 200, response.content
 
-    data = response.json()
+# def test_update_review(session, client, test_user, test_meeting):
+#     update_test_context = "update test context"
 
-    assert data["meeting_id"] == meeting_id
-    assert data["context"] == test_context
+#     test_review = create_test_review(
+#         session=session, meeting_id=test_meeting.id, user_id=test_user.id
+#     )
 
+#     update_review_schema = meeting_schmea.ReviewUpdateIn(context=update_test_context)
 
-def test_update_review(session, client, test_user, test_meeting):
-    update_test_context = "update test context"
+#     json_data = json.loads(update_review_schema.model_dump_json(exclude_unset=True))
 
-    test_review = create_test_review(
-        session=session, meeting_id=test_meeting.id, user_id=test_user.id
-    )
+#     response = client.put(f"v1/review/{test_review['id']}", json=json_data)
 
-    update_review_schema = meeting_schmea.ReviewUpdateIn(context=update_test_context)
+#     assert response.status_code == 200, response.content
 
-    json_data = json.loads(update_review_schema.model_dump_json(exclude_unset=True))
+#     data = response.json()
 
-    response = client.put(f"v1/review/{test_review['id']}", json=json_data)
+#     assert data["context"] == update_test_context
 
-    assert response.status_code == 200, response.content
 
-    data = response.json()
+# def test_delete_review(session, client, test_user, test_meeting):
+#     test_review = create_test_review(
+#         session=session, meeting_id=test_meeting.id, user_id=test_user.id
+#     )
 
-    assert data["context"] == update_test_context
+#     response = client.delete(f"v1/review/{test_review['id']}")
 
-
-def test_delete_review(session, client, test_user, test_meeting):
-    test_review = create_test_review(
-        session=session, meeting_id=test_meeting.id, user_id=test_user.id
-    )
-
-    response = client.delete(f"v1/review/{test_review['id']}")
-
-    assert response.status_code == 200, response.content
+#     assert response.status_code == 200, response.content
