1ce55df99b9bc279ff9ea3e7fa131c080315a260
Lee Hyun Jun <bhk0827@gmail.com>
2024-07-03 12:39:18 +0900

    <Feat> : Session close

--- apps/scheduler_module.py
+++ apps/scheduler_module.py
@@ -10,48 +10,67 @@ from log import scheduler_logger
 
 def meeting_active_check():
     db = SessionLocal()
-    all_active_meeting = crud.meeting.get_all_active_meeting(db=db)
-    current_time = datetime.now()
+    try:
+        all_active_meeting = crud.meeting.get_all_active_meeting(db=db)
+        current_time = datetime.now()
 
-    deactive_count = 0
-    for meeting in all_active_meeting:
-        if meeting.meeting_time < current_time:
-            meeting.is_active = False
-            deactive_count += 1
+        deactive_count = 0
+        for meeting in all_active_meeting:
+            if meeting.meeting_time < current_time:
+                meeting.is_active = False
+                deactive_count += 1
 
-    db.commit()
-    scheduler_logger.warning(f"{deactive_count} mettings deactive")
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        scheduler_logger.error(f"Error While meeting_active_check : {e}")
+    finally:
+        db.close()
+        scheduler_logger.warning(f"{deactive_count} mettings deactive")
     return
 
 
 def user_remove_after_seven():
     db = SessionLocal()
-    deactive_users = crud.user.get_deactive_users(db=db)
-    current_date = datetime.now().date()
+    try:
+        deactive_users = crud.user.get_deactive_users(db=db)
+        current_date = datetime.now().date()
 
-    deleted_count = 0
-    for user in deactive_users:
-        # 사용자의 비활성화 날짜가 현재 날짜보다 일주일 이상 이전인지 확인
-        if user.deactive_time and current_date - user.deactive_time.date() > timedelta(
-            days=7
-        ):
-            db.delete(user)  # 사용자 삭제
-            deleted_count += 1
+        deleted_count = 0
+        for user in deactive_users:
+            # 사용자의 비활성화 날짜가 현재 날짜보다 일주일 이상 이전인지 확인
+            if (
+                user.deactive_time
+                and current_date - user.deactive_time.date() > timedelta(days=7)
+            ):
+                db.delete(user)  # 사용자 삭제
+                deleted_count += 1
 
-    db.commit()
-    scheduler_logger.warning(f"{deleted_count} user(s) deleted")
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        scheduler_logger.error(f"Error While user_remove_after_seven : {e}")
+    finally:
+        db.close()
+        scheduler_logger.warning(f"{deleted_count} user(s) deleted")
     return
 
 
 def meeting_time_alarm():
     db = SessionLocal()
 
-    current_time = datetime.now()
-    meeting_id_list = crud.meeting.get_meeting_with_hour(
-        db=db, current_time=current_time
-    )
+    try:
+        current_time = datetime.now()
+        meeting_id_list = crud.meeting.get_meeting_with_hour(
+            db=db, current_time=current_time
+        )
 
-    for id in meeting_id_list:
-        crud.alarm.meeting_time_alarm(db=db, meeting_id=id)
-
+        for id in meeting_id_list:
+            crud.alarm.meeting_time_alarm(db=db, meeting_id=id)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        scheduler_logger.error(f"Error While meeting_time_alarm : {e}")
+    finally:
+        db.close()
     return None
